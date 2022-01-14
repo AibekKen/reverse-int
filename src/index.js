e4919a253784d0c659fd8eaa173c8b30e719dc70
Aibek <ai.bek@mail.ru>
2022-01-15 00:01:29 +0600

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+   const stringN = Math.abs(n).toString();
+   const arrayN = [...stringN];
+   const reverseN = arrayN.reverse().join(''); 
+   return reverseN;
+ }
